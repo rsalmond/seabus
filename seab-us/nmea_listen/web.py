ca5d5f49790ae05d26cc69133833b4de036e5079
Rob Salmond <rob@salmond.ca>
2016-05-03 04:43:00 +0000

    include a unique id for each boat+telemetry data point we send down

--- seab-us/nmea_listen/web.py
+++ seab-us/nmea_listen/web.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/seabus")
+@app.route("/data/v1")
 def seabus():
     telemetry = {'boats': []}
     for boat in Boat.all():
@@ -11,10 +11,12 @@ def seabus():
             lat = boat.telemetry[0].lat
             lon = boat.telemetry[0].lon
             name = boat.name
+            id = boat.id
             telemetry['boats'].append(
                 {'lat': lat,
                 'lon': lon,
-                'name': name
+                'name': name,
+                'id': id
                 }
             )
 
