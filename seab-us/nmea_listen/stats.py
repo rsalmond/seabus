69781531b99f0184dea7d4ce27b19df8e604220b
rsalmond <rsalmond@appneta.com>
2016-05-04 23:45:18 -0700

    plot some histograms of beacons received WRT distance from tuner

--- seab-us/nmea_listen/stats.py
+++ seab-us/nmea_listen/stats.py
@@ -1,6 +1,24 @@
 from models import Boat, Telemetry
+from pandas import DataFrame
+from haversine import haversine
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
-if __name__ == '__main__':
+
+def go(bus=None):
+    tuner_position = (49.284768, -123.109248)
     seabuses = Boat.all_seabuses()
-    import pdb
-    pdb.set_trace() 
+    telemetry = [rec.__dict__ for rec in seabuses[bus].telemetry]
+    df = DataFrame.from_records(telemetry)
+    df['dist'] = [haversine(pos, tuner_position) for pos in zip(df.lat, df.lon)]
+    return df
+
+if __name__ == '__main__':  
+    bunk = go(1)
+    #bunk['dist'].hist()
+    #bunk[(bunk['nav_status'] == 0)]['dist'].hist()
+    bunk[(bunk['received'] > '2016-05-04 06:00') & (bunk['received'] < '2016-05-05')]['dist'].hist()
+    plt.show()
