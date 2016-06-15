98502da02b89158da0113f5e2b9bb01027433c80
Rob Salmond <rob@salmond.ca>
2016-06-14 23:20:03 -0700

    begin working on geopandas spatial analysis

--- seabus/model/model.py
+++ seabus/model/model.py
@@ -1,19 +1,24 @@
 #from .common.bounding_boxes import WATERFRONT, LONSDALE
 import pandas as pd
+import geopandas as gpd
+from geopandas import GeoSeries, GeoDataFrame
+from geopandas.tools import sjoin
 import sqlite3
 from shapely.geometry import Point
 from ..common.bounding_boxes import bounds
 
 def loadbus(bus=None):
     con = sqlite3.connect('test.db')
-    return pd.read_sql_query('select * from telemetry where boat_id =5', con, parse_dates=['received'], index_col=['received']) 
+    df = pd.read_sql_query('select * from telemetry where boat_id =5', con, parse_dates=['received'], index_col=['received'])
+    # geopandas requires this to be called 'geometry' 
+    df['geometry'] = df.apply(lambda y: Point(y.lon, y.lat), axis=1)
+    return df
 
 def label_dockings(data):
     """ add a label to indicate when the bus was stopped at one of the docks """
     for row in data:
         import pdb
         pdb.set_trace()
-    #data['point'] = Point(data['lat'], data['lon'])
     return data
 
 def label_arrive(data):
@@ -24,7 +29,12 @@ def label_depart(data):
     """ add a label to indicate when the bus departs one of the docks """
 
 if __name__ == '__main__':
+    print 'creating bounds'
+    spots = GeoDataFrame({'geometry': [bounds['WATERFRONT'], bounds['LONSDALE'], bounds['PARKING']]})
+    print 'loading bus data'
     data = loadbus()
-    print label_dockings(data).head()
+    gdf = gpd.GeoDataFrame(data)
+    print 'joining'
+    #joined = sjoin(gdf, spots, how='inner', op='contains')
     import pdb
     pdb.set_trace()
