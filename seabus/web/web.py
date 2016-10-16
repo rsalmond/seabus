011aedec2cc44fa3e4b112eff5a56a40ad06399c
rsalmond <rob@salmond.ca>
2016-10-16 21:17:43 +0000

    [infra] better dev / prod config and cli

--- seabus/web/web.py
+++ seabus/web/web.py
@@ -5,10 +5,14 @@ from seabus.web.blueprint import blueprint
 from seabus.common.database import db
 from seabus.web.socketio import socketio
 
-
-def create_app(config):
+def create_app(config=None):
     app = Flask(__name__)
-    app.config.from_object('seabus.web.config.{}'.format(config))
+
+    if config is not None:
+        app.config.from_object('seabus.web.config.{}'.format(config))
+    else:
+        app.config.from_object('seabus.web.config.Dev')
+
     socketio.init_app(app)
     app.register_blueprint(blueprint)
     db.init_app(app)
