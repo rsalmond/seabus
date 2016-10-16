dc70e1fe562b5e0511c13f1bd727538578d8db54
rsalmond <rob@salmond.ca>
2016-10-16 01:34:57 +0000

    [websocket] move socketio set up out of main web app to avoid circular reference from blueprint

--- seabus/web/socketio.py
+++ seabus/web/socketio.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import
-from seabus.web.web import socketio
+from flask_socketio import SocketIO, emit
+from seabus.common.telemetry import fetch_telemetry
+
+socketio = SocketIO()
 
 @socketio.on('connect', namespace='/seabus_data')
 def on_connect():
-    pass
+    """
+    send initial telemetry on client connect
+    """
+    telemetry = fetch_telemetry()
+    emit('seabus_moved', telemetry)
--- seabus/web/web.py
+++ seabus/web/web.py
@@ -1,17 +1,16 @@
 from flask import Flask
-from flask_socketio import SocketIO
 import oboe
 from oboeware import OboeMiddleware
 from seabus.web.blueprint import blueprint
 from seabus.common.database import db
+from seabus.web.socketio import socketio
 
-socketio = SocketIO()
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object('seabus.web.config.{}'.format(config))
+    socketio.init_app(app)
     app.register_blueprint(blueprint)
     db.init_app(app)
     #TODO: tv_app = OboeMiddleware(app)
-    socketio.init_app(app)
     return app
