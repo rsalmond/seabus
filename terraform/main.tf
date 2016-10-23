2e416b838513462a8d3b8764e0135b8173154241
rsalmond <rsalmond@appneta.com>
2016-10-23 15:09:19 -0700

    [docker] derp, install docker!

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,11 @@ resource "aws_route53_record" "www" {
     ttl         = "300"
     records     = ["107.161.27.162"]
 }
+
+resource "aws_route53_record" "api" {
+    zone_id     = "Z38AGVFAO0JEIN"
+    name        = "api.seab.us"
+    type        = "A"
+    ttl         = "300"
+    records     = ["107.161.27.162"]
+}
