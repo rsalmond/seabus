42381f2864660a1060d8eec4b7a7662a72525b3b
Rob Salmond <rob@salmond.ca>
2016-11-13 20:58:19 -0800

    [docker] prod/dev configs are the same in container land

--- seabus/web/config.py
+++ seabus/web/config.py
@@ -4,22 +4,17 @@ import seabus
 class Config(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////seabus/data/db_seabus.db'
+    LISTENER_HOST = '0.0.0.0'
+    LISTENER_PORT = 3000
+    LISTENER_UPDATE_URL = 'http://web:5000/update'
 
 class Test(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 class Dev(Config):
     DEBUG=True
-    seabus_project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/db_seabus.db'.format(seabus_project_root)
-    LISTENER_HOST = '0.0.0.0'
-    LISTENER_PORT = 3001
-    LISTENER_UPDATE_URL = 'http://dev.seab.us/update'
 
 class Prod(Config):
-    seabus_project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/db_seabus.db'.format(seabus_project_root)
-    LISTENER_HOST = '10.8.0.1'
-    LISTENER_PORT = 3000
-    LISTENER_UPDATE_URL = 'http://seab.us/update'
+    pass
